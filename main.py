6670eeca1ae5b7047510a8fc8bc3052181721bd7
eliseev881@gmail.com <eliseev881@gmail.com>
2024-05-21 22:54:51 +0300

    commit

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((960, 540))
+
+screen_width = 1900
+screen_height = 990
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Kitty Manipulator')
 
 running = True
 while running:
